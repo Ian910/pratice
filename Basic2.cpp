5d0fe9cb6d2e89c064fd4ae276138e0d0c0b9b32
Ian910 <66122637+Ian910@users.noreply.github.com>
2020-05-31 22:13:52 +0800

    Add files via upload

---
+++ Basic2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string name;
+	cout << "請輸入姓名：";
+	cin >> name;
+	cout << "\n歡迎" << name << "\t使用本軟體";
+
+
+
+
+	system("pause");
+	return 0;
+}
