1a379e83ea047cf8e8e7e51acacbf2c5591f24ef
Ian910 <66122637+Ian910@users.noreply.github.com>
2020-05-31 21:11:32 +0800

    Add files via upload

---
+++ Basic1/Basic1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "方文山\t座號為" << 14 << "號" << "生日為20010925\n";
+	cout << "陳品錞\t座號為" << 27 << "號" << "生日為20020325\n";
+	cout << "江雨曈\t座號為" << 14 << "號" << "生日為20020328\n";
+	system("pause");
+	return 0;
+}
