d532415adac375abf719a9cdac12e12362021c9c
Ian910 <66122637+Ian910@users.noreply.github.com>
2020-06-03 22:55:15 +0800

    Add files via upload

---
+++ Basic6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string number;
+	cout << "請輸入一分鐘打字字數：";
+	cin >> number;
+	if (number >= "80")
+	{
+		cout << "你是打字高手!\n";
+	}
+
+
+
+	system("pause");
+	return 0;
+}
