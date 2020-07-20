d1d4ff15a53752604af1ae93ed6ddfa80dd6cb02
Ian910 <66122637+Ian910@users.noreply.github.com>
2020-07-20 21:02:10 +0800

    Update Basic2.cpp

--- Basic2.cpp
+++ Basic2.cpp
@@ -16,10 +16,10 @@ using namespace std;
 
 int main()
 {
-	string name;
-	cout << "請輸入姓名：";
-	cin >> name;
-	cout << "\n歡迎" << name << "\t使用本軟體";
+	char name[100];
+	printf("請輸入姓名：");
+	scanf_s("%s",&name);
+	printf("\n歡迎,%s,\t使用本軟體", name);	
 	system("pause");
 	return 0;
 }
