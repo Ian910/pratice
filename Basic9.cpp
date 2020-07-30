511c27ab67d028a86c3a2782bb4d68e26687e59a
Ian910 <66122637+Ian910@users.noreply.github.com>
2020-07-30 21:45:37 +0800

    Update Basic9.cpp

--- Basic9.cpp
+++ Basic9.cpp
@@ -19,3 +19,26 @@ int main()
 	system("pause");
 	return 0;
 }
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (int i = 1; i <= 5; i++)
+	{
+		int n;
+		float ave,sum = 0.00;
+		printf("第%d個班級\n",i);
+		for (int j = 1; j <= 4; j++)
+		{
+			printf("請輸入第%d位學生的成績:",j);
+			scanf_s("%d",&n);
+			sum += n;
+			ave = sum / 4;
+		}
+		printf("第%d班總成績為:%0.0f分,平均為%-1.2f分\n",i,sum,ave);
+	}
+	system("pause");
+	return 0;
+}
