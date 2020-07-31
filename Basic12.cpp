e267b8dfd437e565ce3f5c266a664eff14099d4a
Ian910 <66122637+Ian910@users.noreply.github.com>
2020-07-31 21:13:23 +0800

    Update Basic12.cpp

--- Basic12.cpp
+++ Basic12.cpp
@@ -26,3 +26,32 @@ int main()
 	system("pause");
 	return 0;
 }
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int k, n, j;
+	float sum = 0.00, ave = 0;
+	printf( "請輸入缺考學生的座號:");
+	scanf_s("%d",&k);
+	for (j = 1; j >= 0; j++)
+	{
+		if (j == k)
+		{
+			continue;
+		}
+		printf( "請輸入第%d號的成績:",j);
+		scanf_s("%d",&n);
+		if (n == -1)
+		{
+			break;
+		}
+		sum += n;
+	}
+	ave = sum / (j - 1);
+	printf("全班成績為%0.2f分, 平均為%0.2f分",sum,ave); 
+	system("pause");
+	return 0;
+}
